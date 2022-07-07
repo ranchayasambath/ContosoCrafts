e04464fa5b07ff7360714ce9a178ddcbdb24b0d6
SambathRanchaya <SambathRanchaya@DESKTOP-2E39V1G>
2022-07-07 16:37:12 -0500

    Add project files.

---
+++ ContosoCrafts.WebSite/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ContosoCrafts.WebSite.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Maker { get; set; }
+
+        [JsonPropertyName("img")]
+
+        public string Url { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public override string ToString() =>
+            JsonSerializer.Serialize<Product>(this);
+
+    }
+}
